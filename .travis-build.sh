dd25e263c76e0eefed6d20c19ee7bfd4922aa09e
Davide Cesari <dcesari@arpae.it>
2021-05-25 15:47:41 +0200

    adapt to centos8 build image

--- .travis-build.sh
+++ .travis-build.sh
@@ -3,7 +3,7 @@ set -exo pipefail
 
 image=$1
 
-if [[ $image =~ ^centos: ]]
+if [[ $image =~ ^centos:7 ]]
 then
     pkgcmd="yum"
     builddep="yum-builddep"
@@ -11,8 +11,18 @@ then
     yum install -q -y epel-release
     yum install -q -y @buildsys-build
     yum install -q -y yum-utils
-#    yum install -q -y git
     yum install -q -y rpmdevtools
+elif [[ $image =~ ^centos:8 ]]
+then
+    pkgcmd="dnf"
+    builddep="dnf builddep"
+    sed -i '/^tsflags=/d' /etc/dnf/dnf.conf
+    dnf install -q -y epel-release
+    dnf install -q -y 'dnf-command(config-manager)'
+    dnf config-manager --set-enabled powertools
+    dnf groupinstall -q -y "Development Tools"
+    dnf install -q -y 'dnf-command(builddep)'
+    dnf install -q -y rpmdevtools
 elif [[ $image =~ ^fedora: ]]
 then
     pkgcmd="dnf"
@@ -20,7 +30,6 @@ then
     sed -i '/^tsflags=/d' /etc/dnf/dnf.conf
     dnf install -q -y @buildsys-build
     dnf install -q -y 'dnf-command(builddep)'
-#    dnf install -q -y git
     dnf install -q -y rpmdevtools
 fi
 
